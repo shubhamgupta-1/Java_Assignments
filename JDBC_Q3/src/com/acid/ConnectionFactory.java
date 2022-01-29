7525d424602c7a375e015e07d458250a72606349
shubhamgupta-1 <55880340+shubhamgupta-1@users.noreply.github.com>
2022-01-29 04:36:05 -0800

    Add files via upload
    JDBC complete

---
+++ JDBC_Q3/src/com/acid/ConnectionFactory.java
@@ -0,0 +1,46 @@
+package com.acid;
+
+import java.io.*;
+import java.sql.*;
+import java.util.Properties;
+
+public class ConnectionFactory {
+
+	private static Connection connection = null;
+
+	public static Connection getConnection() {
+
+		Properties properties = new Properties(); // special types of hashmap to fetch details from db.properties
+
+		InputStream ins = null;
+		try {
+			ins = new FileInputStream("db.properties");
+
+			properties.load(ins);
+
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		String url = properties.getProperty("jdbc.url");
+		String driverName = properties.getProperty("jdbc.driver");
+		String userName = properties.getProperty("jdbc.username");
+		String password = properties.getProperty("jdbc.password");
+
+		try {
+			Class.forName(driverName);
+			System.out.println("driver is loaded...");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			connection = DriverManager.getConnection(url, userName, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+}
